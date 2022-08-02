8d66a44c7acfd4434bd62652c54d266f60456f6e
Alex Leon <alejandroleon5434@gmail.com>
2022-08-01 17:52:57 -0700

    feat(server): setup mongoose

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const PORT = 8000;
 
 // setup express
 
 app.listen(PORT, () => console.log(`listening at: http://localhost:${PORT}`));
+
+// setup mongoose
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/jwt-auth",
+  (err) => {
+    if (err) throw err;
+    console.log("MongoDB connection established");
+  }
+);
